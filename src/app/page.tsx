eae19ed672be7baaf917bdfb85ab4abaf088d082
Valentina Trebbi <valentinatrebbi97@gmail.com>
2025-02-01 22:19:28 +0100

    - modifica css
    - modifica main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
+import { GridRow, GridColumn, Grid, Card, CardHeader, CardContent, Input, Button } from 'semantic-ui-react';
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        main
-      </main>
-    </div>
+    <main>
+      <Grid>
+        <GridRow columns={1} className='main'>
+          <GridColumn textAlign='center'>
+            <Card className='basic-card'>
+              <CardHeader>
+                <p style={{fontSize: '2rem'}}>Benvenuto!</p>
+              </CardHeader>
+              <CardContent>
+                <Input  placeholder="Nome Sala" size='big'></Input>
+                <button style={{marginTop: '1rem', marginRight: 0}} className="ui button green big">Crea</button>
+              </CardContent>
+              <CardContent extra>
+                <p style={{fontSize: '1.5rem'}}>sale disponibili:</p>
+              </CardContent>
+            </Card>
+          </GridColumn>
+        </GridRow>
+      </Grid>
+
+    </main>
   );
 }
