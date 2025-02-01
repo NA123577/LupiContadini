90679d697260b9ab0ef666398c6f3ad97488ac34
Valentina Trebbi <valentinatrebbi97@gmail.com>
2025-02-01 20:49:55 +0100

    - aggiubnto logo
    - modificato css
    
    - modificato header

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,5 +1,15 @@
+"use client";
+import { GridRow, GridColumn, Grid, Image } from 'semantic-ui-react';
+
 export default function Header() {
-    return (<header>
-        ciao header
-    </header>)
+    return (
+        <Grid padded>
+            <GridRow className='header-nav' key={'red'} columns={2}>
+                <GridColumn textAlign='left' verticalAlign='middle'>
+                    <Image className='logo' src='Logo.png'/>
+                </GridColumn>
+                <GridColumn textAlign='right' verticalAlign='middle'>Lupi Contadini!</GridColumn>
+            </GridRow>
+        </Grid>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { Rating } from 'semantic-ui-react';
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Rating icon='star' defaultRating={3} maxRating={4} />
         main
       </main>
     </div>
