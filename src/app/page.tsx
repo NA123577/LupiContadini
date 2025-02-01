70ede179c6c64ede3de3ef345394e56898d2afab
Valentina Trebbi <valentinatrebbi97@gmail.com>
2025-02-01 18:54:13 +0100

    test rating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { Rating } from 'semantic-ui-react';
 
 export default function Home() {
   return (
     <div>
       <main>
-       main
+        <Rating icon='star' defaultRating={3} maxRating={4} />
+        main
       </main>
     </div>
   );
